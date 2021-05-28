0a62b02eccdcdfcdf3ed956c64575b0db07cda34
tre3ubec <ekoroteev27@yandex.ru>
2021-05-28 16:22:46 +0300

    hello world!

---
+++ environment/src/Hello.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import "./Hello.css";
+
+class Hello extends Component {
+    render() {
+        return (
+            <div className="helloContainer">
+                <h1>Hello World!</h1>
+            </div>
+        );
+    }
+}
+export default Hello;

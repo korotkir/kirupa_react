972cc08e372a9eb18f871ed32248fafe6a33c4f1
tre3ubec <ekoroteev27@yandex.ru>
2021-06-03 16:11:29 +0300

    Chapter 18. Router. 1/2

---
+++ react_spa/src/Contact.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react"
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h2>QUESTINS?</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, voluptas.</p>
+            </div>
+        )
+    }
+}
+
+export default Contact
---
+++ react_spa/src/Main.js
@@ -0,0 +1,30 @@
+import React, {Component} from "react"
+import {
+    Route,
+    NavLink,
+    HashRouter
+} from "react-router-dom"
+import Home from "./Home"
+import Stuff from "./Stuff"
+import Contact from "./Contact"
+
+class Main extends Component {
+    render() {
+        return (
+            <HashRouter>
+                <div>
+                    <h1>Simple SPA</h1>
+                    <ul className="header">
+                        <li><NavLink to="/stuff">Main</NavLink></li>
+                        <li><NavLink to="/stuff">Products</NavLink></li>
+                        <li><NavLink to="/contact">Contact</NavLink></li>
+                    </ul>
+                    <div className="content">
+                    </div>
+                </div>
+            </HashRouter>
+        )
+    }
+}
+
+export default Main

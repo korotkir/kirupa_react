dd1493800d545fdd767ac47707b9c71c7a14cc19
tre3ubec <ekoroteev27@yandex.ru>
2021-06-09 18:36:16 +0300

    Chapter 20. Redux with Redux. Finally.

---
+++ redux_counter/src/App.js
@@ -0,0 +1,31 @@
+import {connect} from "react-redux"
+import Counter from "./Counter"
+
+// Сопоставление состояния Redux со свойствами компонента
+function mapStateToProps(state) {
+ return {
+  countValue: state.count
+ }; }
+
+// Действие
+let increaseAction = {type: "increase"}
+let decreaseAction = {type: "decrease"}
+
+// Сопоставление действий Redux со свойствами компонента
+function mapDispatchToProps(dispatch) {
+ return {
+  increaseCount: function() {
+   return dispatch(increaseAction);
+  },
+  decreaseCount: function() {
+   return dispatch(decreaseAction);
+  } };
+}
+
+
+// HOC-компонент
+let connectedComponent = connect(
+ mapStateToProps,
+ mapDispatchToProps
+)(Counter);
+export default connectedComponent;
